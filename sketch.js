8bc76cd92c05e808f5e367a4a6c83987d87433c7
SanthoshMohanavelu <85075055+SanthoshMohanavelu@users.noreply.github.com>
2021-08-11 16:00:28 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var backgroundImage, bgImg, car1_img, car2_img, track;
 var database, gameState;
 var form, player, playerCount;
 var allPlayers, car1, car2;
-var cars = [];
+var cars = []
 
 function preload() {
   backgroundImage = loadImage("./assets/background.png");
